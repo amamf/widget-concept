644931e77b268632760748aa6e2f8aebcb59ff55
amamf <amamf@users.noreply.github.com>
2015-09-21 01:14:49 +0300

    added news title

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,50 +2,33 @@
 
 define(['services/route-resolver'], function () {
 
-  //  angular.module('widgetConceptApp.common', []);
-//  angular.module('widgetConceptApp.communication', []);
-//  angular.module('widgetConceptApp.models', []);
-//  angular.module('widgetConceptApp.widgets', []);
-//  angular.module('widgetConceptApp.data', []);
-//  angular.module('widgetConceptApp.news', [
-//    'widgetConceptApp.data',
-//    'widgetConceptApp.communication',
-//    'widgetConceptApp.models',
-//    'widgetConceptApp.common'
-//  ]);
-
   var app = angular.module('widgetConceptApp', [
-      'ngRoute',
-      'ngSanitize',
-      'widgetConceptApp.routing'
-//      'widgetConceptApp.communication'
-//      'widgetConceptApp.widgets',
-//      'widgetConceptApp.data',
-//      'widgetConceptApp.news'
-    ]);
+    'ngRoute',
+    'ngSanitize',
+    'widgetConceptApp.routing'
+  ]);
 
-  app.config(function ($routeProvider, routeResolverProvider, $controllerProvider,
-                      $compileProvider, $filterProvider, $provide) {
+  app.config(function ($routeProvider, routeResolverProvider, $controllerProvider, $compileProvider, $filterProvider, $provide) {
 
-      app.register = {
-        controller: $controllerProvider.register,
-        directive: $compileProvider.directive,
-        filter: $filterProvider.register,
-        factory: $provide.factory,
-        service: $provide.service
-      };
+    app.register = {
+      controller: $controllerProvider.register,
+      directive: $compileProvider.directive,
+      filter: $filterProvider.register,
+      factory: $provide.factory,
+      service: $provide.service
+    };
 
-      routeResolverProvider.routeConfig.setBaseDirectories('/views/', '/scripts/controllers/');
+    routeResolverProvider.routeConfig.setBaseDirectories('/views/', '/scripts/controllers/');
 
-      var route = routeResolverProvider.route;
+    var route = routeResolverProvider.route;
 
-      $routeProvider
-        .when('/', route.resolve('main'))
-        .when('/news', route.resolve('news'))
-        .otherwise({
-          redirectTo: '/'
-        });
-    });
+    $routeProvider
+      .when('/', route.resolve('main'))
+      .when('/news', route.resolve('news', '', 'news'))
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
 
   return app;
 });
